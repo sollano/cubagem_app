7324f9d1f22f2a80b04b9ca3f5470fdd0419b342
Sollano R Braga <sollanorb@gmail.com>
2018-02-11 04:27:51 -0200

    function now keeps the data's original names

--- funs/cub_summary.R
+++ funs/cub_summary.R
@@ -33,28 +33,37 @@ cub_summary <- function(df, dap, ht, vcc, vsc, .groups){
     stop(".groups not set", call. = F) 
   }
   
+  dap_name <- dap
+  ht_name <- ht
+  vcc_name <- vcc
+  vsc_name <- vsc
+  
   # funcao para transformar strings em symmbolos que o dplyr entende
   sym <- rlang::sym 
-  dap <- sym(dap) 
-  ht  <- sym(ht)
-  vcc <- sym(vcc)
-  vsc <- sym(vsc)
+  dap_sym <- sym(dap) 
+  ht_sym  <- sym(ht)
+  vcc_sym <- sym(vcc)
+  vsc_sym <- sym(vsc)
+  
+  # !! diz para o dplyr que voce esta lidando com simbolos ou strings
+  
+  # := e utilizado quando o nome da variavel nova dentro do pipe esta dentro de um objeto
   
   df  %>%                                     # define data frame utilizado
     na_if(0) %>%                              # Transforma zeros em NA
     group_by_at( vars(.groups) )  %>%         # definicao da chave
     summarize(                                # Funcao que compila os df
-      DAP        = mean(!!dap, na.rm = TRUE), # Media de DAP
-      HT         = mean(!!ht,  na.rm = TRUE), # media de HT
-      AS         = pi * DAP^2 / 40000       , # Area Seccional
-      VCC        = sum(!!vcc,  na.rm = TRUE), # Soma de volume com casca
-      VSC        = sum(!!vsc,  na.rm = TRUE), # Soma de volume sem casca
-      PORC_CASCA = ((VCC - VSC)/VCC)*100    , # Porcentagem da casca
-      #  VCIL    = AS * HT,
-      FFCC       = VCC / (AS * HT )         , # Fator de forma com casca
-      FFSC       = VSC / (AS * HT ) ) %>%     # Fator de forma sem casca
+      !!dap_name := mean(!!dap_sym, na.rm = TRUE), # Media de DAP
+      !!ht_name  := mean(!!ht_sym,  na.rm = TRUE), # media de HT
+      AS          = pi * (!!sym(dap_name))^2 / 40000       , # Area Seccional
+      !!vcc_name := sum(!!vcc_sym,  na.rm = TRUE), # Soma de volume com casca
+      !!vsc_name := sum(!!vsc_sym,  na.rm = TRUE), # Soma de volume sem casca
+      PORC_CASCA = (( (!!sym(vcc_name)) - (!!sym(vsc_name)) )/ (!!sym(vcc_name))  )*100    , # Porcentagem da casca
+      #  VCIL    = AS *  (!!sym(ht_name)) ,
+      FFCC       = (!!sym(vcc_name)) / (AS * (!!sym(ht_name)) )   , # Fator de forma com casca
+      FFSC       = (!!sym(vsc_name)) / (AS * (!!sym(ht_name)) )   ) %>%     # Fator de forma sem casca
     mutate_at(                                # Funcao que cria novas variaveis utilizando as variaveis
-      vars(contains("FF")),                   # especificadas por vars
+      vars(FFCC, FFSC),                   # especificadas por vars
       funs(medio = mean)    ) %>%             # Fator de forma medio
     na_if(0) %>%                              # Se vsc nao for informado, variaveis que o utilizam serao 0, portanto, deve-se converte-las para NA, para depois remove-las
     select_if(Negate(anyNA))                  # remove variaveis que nao foram informadas (argumentos opicionais nao inseridos viram NA)
