0d165a7c31d9dabdbdbeebdffac77a07ba531cd1
sollano <sollanorb@gmail.com>
2018-04-22 00:09:51 -0300

    bad error messages

--- server.R
+++ server.R
@@ -558,8 +558,9 @@ shinyServer(function(input, output, session){
     validate(check_dap_min(nm$diam.min,max.val)) 
     
     # Caso nao ultrapasse, filtrar
+    if(!is.na(nm$diam.min)){
     data <- data[data[nm$dap]>=nm$diam.min, ] 
-    
+    }
     
     
     # o proximo if sera para filtrar as linhas
